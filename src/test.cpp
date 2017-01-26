95ee57361d9cf0c6268af23efd2f8bcc2f793b99
Christian Blume <chr.blume@gmail.com>
2017-01-27 00:08:19 +1300

    add initial graph creation

--- src/test.cpp
+++ src/test.cpp
@@ -1,21 +1,36 @@
 #include <libunittest/all.hpp>
 #include "transwarp.h"
+#include <fstream>
 
 
 COLLECTION(test_transwarp) {
+
+std::string make_dot(const std::vector<transwarp::edge>& graph) {
+    auto info = [](transwarp::node n) {
+        return '"' + std::to_string(n.id) + "_" + n.name + '"';
+    };
+    std::ostringstream ofile;
+    ofile << "digraph transwarp {" << std::endl;
+    for (auto pair : graph) {
+        ofile << info(pair.parent) << " -> " << info(pair.child) << std::endl;
+    }
+    ofile << "}" << std::endl;
+    return ofile.str();
+}
 
 TEST(basic) {
     int value = 42;
 
     auto f1 = [&value]{ return value; };
-    auto task1 = transwarp::make_task("f1", f1);
+    auto task1 = transwarp::make_task("t1", f1);
 
     auto f2 = [](int v) { return v + 2; };
-    auto task2 = transwarp::make_task("f2", f2, task1);
+    auto task2 = transwarp::make_task("t2", f2, task1);
 
     auto f3 = [](int v, int w) { return v + w + 3; }; 
-    auto root_task = transwarp::make_task("f3", f3, task1, task2);
+    auto root_task = transwarp::make_task("t3", f3, task1, task2);
 
+    root_task->make_ids();
     root_task->set_parallel(4);
 
     root_task->schedule();
@@ -26,6 +41,33 @@ TEST(basic) {
     root_task->reset();
     root_task->schedule();
     ASSERT_EQUAL(91, root_task->future().get());
+
+    std::ofstream ofile("basic.dot");
+    ofile << make_dot(root_task->graph());
+}
+
+TEST(graph) {
+    auto f0 = []{ return 0; };
+    auto f1 = [](int){ return 0; };
+    auto f2 = [](int, int){ return 0; };
+    auto f3 = [](int, int, int){ return 0; };
+    auto task1 = transwarp::make_task("task1", f0);
+    auto task2 = transwarp::make_task("task2", f1, task1);
+    auto task3 = transwarp::make_task("task3", f1, task2);
+    auto task4 = transwarp::make_task("task4", f2, task2, task1);
+    auto task5 = transwarp::make_task("task5", f2, task3, task2);
+    auto task6 = transwarp::make_task("task6", f3, task1, task2, task5);
+    auto task7 = transwarp::make_task("task7", f2, task5, task6);
+    auto task8 = transwarp::make_task("task8", f2, task6, task7);
+    auto task9 = transwarp::make_task("task9", f2, task8, task7);
+    auto task10 = transwarp::make_task("task10", f2, task9, task8);
+    auto task11 = transwarp::make_task("task11", f2, task10, task7);
+    auto task12 = transwarp::make_task("task12", f2, task11, task6);
+    auto root = transwarp::make_task("root", f3, task10, task11, task12);
+
+    root->make_ids();
+    std::ofstream ofile("graph.dot");
+    ofile << make_dot(root->graph());
 }
 
 }
--- src/transwarp.h
+++ src/transwarp.h
@@ -4,138 +4,183 @@
 #include <memory>
 #include <tuple>
 #include <string>
+#include <cstddef>
+#include <vector>
 #include "cxxpool.h"
 
 
 namespace transwarp {
+
+
+struct node {
+    std::size_t id;
+    std::string name;
+};
+
+
+struct edge {
+    node child;
+    node parent;
+};
+
+
 namespace detail {
 
-    template<bool Done, int Total, int... N>
-    struct call_impl {
-        template<typename Result, typename F, typename Tuple>
-        static Result call(F&& f, Tuple&& t) {
-            return call_impl<Total == 1 + sizeof...(N), Total, N..., sizeof...(N)>::template
-                    call<Result>(std::forward<F>(f), std::forward<Tuple>(t));
-        }
-    };
-
-    template<int Total, int... N>
-    struct call_impl<true, Total, N...> {
-        template<typename Result, typename F, typename Tuple>
-        static Result call(F&& f, Tuple&& t) {
-            return std::forward<F>(f)(std::get<N>(std::forward<Tuple>(t))->future().get()...);
-        }
-    };
-
+template<bool Done, int Total, int... N>
+struct call_impl {
     template<typename Result, typename F, typename Tuple>
-    Result call(F&& f, Tuple&& t) {
-        using ttype = typename std::decay<Tuple>::type;
-        return detail::call_impl<0 == std::tuple_size<ttype>::value, std::tuple_size<ttype>::value>::template
+    static Result call(F&& f, Tuple&& t) {
+        return call_impl<Total == 1 + sizeof...(N), Total, N..., sizeof...(N)>::template
                 call<Result>(std::forward<F>(f), std::forward<Tuple>(t));
     }
+};
 
-    template<size_t ...> struct indices
-    {};
-
-    template<size_t ...> struct construct_range;
-
-    template< size_t end, size_t idx, size_t ...i >
-    struct construct_range<end, idx, i... >
-         : construct_range<end, idx+1, i..., idx> {};
-
-    template< size_t end, size_t ...i >
-    struct construct_range< end, end, i... > {
-        typedef indices< i... > type;
-    };
-
-    template<size_t b, size_t e>
-    struct index_range {
-        typedef typename construct_range<e, b>::type type;
-    };
-
-    template<typename F, typename T, typename ...Args>
-    void tuple_for_each_index(indices<>, const F&, T&, const Args&...)
-    {}
-
-    template<typename F, typename T, typename ...Args>
-    void tuple_for_each_index(indices<>, const F&, const T&, const Args&...)
-    {}
-
-    template<size_t i, size_t ...j, typename F, typename T, typename ...Args>
-    void tuple_for_each_index(indices<i,j...>, const F& f, T& t, const Args&... args) {
-        f(std::get<i>(t), args...);
-        detail::tuple_for_each_index(indices<j...>(), f, t, args...);
+template<int Total, int... N>
+struct call_impl<true, Total, N...> {
+    template<typename Result, typename F, typename Tuple>
+    static Result call(F&& f, Tuple&& t) {
+        return std::forward<F>(f)(std::get<N>(std::forward<Tuple>(t))->future().get()...);
     }
+};
 
-    template<size_t i, size_t ...j, typename F, typename T, typename ...Args>
-    void tuple_for_each_index(indices<i,j...>, const F& f, const T& t, const Args&... args) {
-        f(std::get<i>(t), args...);
-        detail::tuple_for_each_index(indices<j...>(), f, t, args...);
-    }
-
-    template<typename F, typename T, typename ...Args>
-    void apply(const F& f, T& t, const Args&... args) {
-        static const size_t n = std::tuple_size<T>::value;
-        typedef typename index_range<0,n>::type index_list;
-        detail::tuple_for_each_index(index_list(), f, t, args...);
-    }
-
-    template<typename F, typename T, typename ...Args>
-    void apply(const F& f, const T& t, const Args&... args) {
-        static const size_t n = std::tuple_size<T>::value;
-        typedef typename index_range<0,n>::type index_list;
-        detail::tuple_for_each_index(index_list(), f, t, args...);
-    }
-
-    struct schedule_functor {
-        template<typename Task>
-        void operator()(std::shared_ptr<Task> task) const {
-            task->schedule();
-        }
-    };
-
-    struct reset_functor {
-        template<typename Task>
-        void operator()(std::shared_ptr<Task> task) const {
-            task->reset();
-        }
-    };
-
-    struct wait_functor {
-        template<typename Task>
-        void operator()(std::shared_ptr<Task> task) const {
-            task->wait();
-        }
-    };
-
-    struct set_thread_pool_functor {
-        explicit set_thread_pool_functor(std::shared_ptr<cxxpool::thread_pool> pool)
-        : pool_(std::move(pool))
-        {}
-        template<typename Task>
-        void operator()(std::shared_ptr<Task> task) const {
-            task->pool_ = pool_;
-        }
-        std::shared_ptr<cxxpool::thread_pool> pool_;
-    };
-
+template<typename Result, typename F, typename Tuple>
+Result call(F&& f, Tuple&& t) {
+    using ttype = typename std::decay<Tuple>::type;
+    return detail::call_impl<0 == std::tuple_size<ttype>::value, std::tuple_size<ttype>::value>::template
+            call<Result>(std::forward<F>(f), std::forward<Tuple>(t));
 }
+
+template<size_t ...> struct indices
+{};
+
+template<size_t ...> struct construct_range;
+
+template< size_t end, size_t idx, size_t ...i >
+struct construct_range<end, idx, i... >
+     : construct_range<end, idx+1, i..., idx> {};
+
+template< size_t end, size_t ...i >
+struct construct_range< end, end, i... > {
+    typedef indices< i... > type;
+};
+
+template<size_t b, size_t e>
+struct index_range {
+    typedef typename construct_range<e, b>::type type;
+};
+
+template<typename F, typename T, typename ...Args>
+void tuple_for_each_index(indices<>, const F&, T&, const Args&...)
+{}
+
+template<typename F, typename T, typename ...Args>
+void tuple_for_each_index(indices<>, const F&, const T&, const Args&...)
+{}
+
+template<size_t i, size_t ...j, typename F, typename T, typename ...Args>
+void tuple_for_each_index(indices<i,j...>, const F& f, T& t, const Args&... args) {
+    f(std::get<i>(t), args...);
+    detail::tuple_for_each_index(indices<j...>(), f, t, args...);
+}
+
+template<size_t i, size_t ...j, typename F, typename T, typename ...Args>
+void tuple_for_each_index(indices<i,j...>, const F& f, const T& t, const Args&... args) {
+    f(std::get<i>(t), args...);
+    detail::tuple_for_each_index(indices<j...>(), f, t, args...);
+}
+
+template<typename F, typename T, typename ...Args>
+void apply(const F& f, T& t, const Args&... args) {
+    static const size_t n = std::tuple_size<T>::value;
+    typedef typename index_range<0,n>::type index_list;
+    detail::tuple_for_each_index(index_list(), f, t, args...);
+}
+
+template<typename F, typename T, typename ...Args>
+void apply(const F& f, const T& t, const Args&... args) {
+    static const size_t n = std::tuple_size<T>::value;
+    typedef typename index_range<0,n>::type index_list;
+    detail::tuple_for_each_index(index_list(), f, t, args...);
+}
+
+struct schedule_functor {
+    template<typename Task>
+    void operator()(std::shared_ptr<Task> task) const {
+        task->schedule();
+    }
+};
+
+struct reset_functor {
+    template<typename Task>
+    void operator()(std::shared_ptr<Task> task) const {
+        task->reset();
+    }
+};
+
+struct wait_functor {
+    template<typename Task>
+    void operator()(std::shared_ptr<Task> task) const {
+        task->wait();
+    }
+};
+
+struct set_thread_pool_functor {
+    explicit set_thread_pool_functor(std::shared_ptr<cxxpool::thread_pool> pool)
+    : pool_(std::move(pool))
+    {}
+    template<typename Task>
+    void operator()(std::shared_ptr<Task> task) const {
+        task->set_thread_pool(pool_);
+    }
+    std::shared_ptr<cxxpool::thread_pool> pool_;
+};
+
+struct make_graph_functor {
+    make_graph_functor(std::vector<edge>& graph, node before)
+    : graph_(graph), before_(std::move(before))
+    {}
+    template<typename Task>
+    void operator()(std::shared_ptr<Task> task) const {
+        task->make_graph(graph_, before_);
+    }
+    std::vector<edge>& graph_;
+    node before_;
+};
+
+struct make_id_functor {
+    explicit make_id_functor(std::size_t id)
+    : id_(id)
+    {}
+    template<typename Task>
+    void operator()(std::shared_ptr<Task> task) const {
+        task->make_id(id_);
+    }
+    std::size_t id_;
+};
+
+} // detail
+
 
 template<typename Functor, typename... Tasks>
 class task : public std::enable_shared_from_this<task<Functor, Tasks...>> {
 public:
+
     using result_type = typename std::result_of<Functor(typename Tasks::result_type...)>::type;
 
     task(std::string name, Functor functor, std::shared_ptr<Tasks>... tasks)
-    : name_(std::move(name)),
+    : node_{0, std::move(name)},
       functor_(std::move(functor)),
       tasks_(std::make_tuple(std::move(tasks)...))
     {}
 
+    void make_ids() {
+        make_id(0);
+    }
+
     void set_parallel(std::size_t n_threads) {
         if (n_threads > 0 && !pool_) {
-            pool_ = std::make_shared<cxxpool::thread_pool>(n_threads);
-            detail::apply(detail::set_thread_pool_functor(pool_), tasks_);
+            auto pool = std::make_shared<cxxpool::thread_pool>(n_threads);
+            set_thread_pool(std::move(pool));
         }
     }
 
@@ -168,15 +213,39 @@ public:
             future_.wait();
     }
 
+    std::vector<edge> graph() const {
+        std::vector<edge> graph;
+        make_graph(graph, {}, true);
+        return graph;
+    }
+
 private:
 
     friend struct detail::set_thread_pool_functor;
+    friend struct detail::make_graph_functor;
+    friend struct detail::make_id_functor;
 
     static result_type evaluate(std::shared_ptr<task> task) {
         return detail::call<result_type>(task->functor_, task->tasks_);
     }
 
-    std::string name_;
+    void make_id(std::size_t id) {
+        node_.id = id++;
+        detail::apply(detail::make_id_functor(id), tasks_);
+    }
+
+    void make_graph(std::vector<edge>& graph, node before, bool is_root=false) const {
+        if (!is_root)
+            graph.push_back({before, node_});
+        detail::apply(detail::make_graph_functor(graph, node_), tasks_);
+    }
+
+    void set_thread_pool(std::shared_ptr<cxxpool::thread_pool> pool) {
+        detail::apply(detail::set_thread_pool_functor(pool), tasks_);
+        pool_ = std::move(pool);
+    }
+
+    node node_;
     Functor functor_;
     std::tuple<std::shared_ptr<Tasks>...> tasks_;
     std::shared_ptr<cxxpool::thread_pool> pool_;
@@ -189,4 +258,5 @@ std::shared_ptr<task<Functor, Tasks...>> make_task(std::string name, Functor fun
     return std::make_shared<task<Functor, Tasks...>>(std::move(name), std::move(functor), std::move(tasks)...);
 }
 
-}
+
+} // transwarp
