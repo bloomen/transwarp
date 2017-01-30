342645dde4fd20e5c2e82fab4c21314b4f813eda
Christian Blume <chr.blume@gmail.com>
2017-01-30 13:03:45 +1300

    improve task scheduling to be non-blocking

--- src/transwarp.h
+++ src/transwarp.h
@@ -16,7 +16,6 @@
 
 // TODOs
 // - write tests
-// - avoid pause/resume of pool (need to push tasks in level order)
 
 
 namespace transwarp {
@@ -26,13 +25,13 @@ struct node {
     std::size_t id;
     std::size_t level;
     std::string name;
-    std::vector<node*> parents;
+    std::vector<const node*> parents;
 };
 
 
 struct edge {
-    transwarp::node child;
-    transwarp::node parent;
+    const transwarp::node* child;
+    const transwarp::node* parent;
 };
 
 
@@ -59,44 +58,16 @@ public:
 
 namespace detail {
 
-template<typename Index, Index max = std::numeric_limits<Index>::max()>
-class infinite_counter {
+class priority_functor {
 public:
-    infinite_counter()
-    : count_{0} {}
 
-    infinite_counter& operator++() {
-        if (count_.back() == max)
-            count_.push_back(0);
-        else
-            ++count_.back();
-        return *this;
-    }
-
-    bool operator>(const infinite_counter& other) const {
-        if (count_.size() == other.count_.size()) {
-            return count_.back() > other.count_.back();
-        } else {
-            return count_.size() > other.count_.size();
-        }
-    }
-
-private:
-    std::vector<Index> count_;
-};
-
-class priority_task {
-public:
-    typedef std::size_t counter_elem_t;
-
-    priority_task()
+    priority_functor()
     : callback_{}, priority_{}, order_{} {}
 
-    priority_task(std::function<void()> callback, std::size_t priority,
-                  transwarp::detail::infinite_counter<counter_elem_t> order)
-    : callback_{std::move(callback)}, priority_(priority), order_{std::move(order)} {}
+    priority_functor(std::function<void()> callback, std::size_t priority, std::size_t order)
+    : callback_{std::move(callback)}, priority_{priority}, order_{order} {}
 
-    bool operator<(const priority_task& other) const {
+    bool operator<(const priority_functor& other) const {
         if (priority_ == other.priority_) {
             return order_ > other.order_;
         } else {
@@ -111,161 +82,75 @@ public:
 private:
     std::function<void()> callback_;
     std::size_t priority_;
-    transwarp::detail::infinite_counter<counter_elem_t> order_;
+    std::size_t order_;
 };
 
 class thread_pool {
 public:
-    /**
-     * Constructor. Launches the desired number of threads
-     * @param n_threads The number of threads to launch.
-     */
+
     explicit thread_pool(std::size_t n_threads)
-    : done_{false}, paused_{false}, threads_{}, tasks_{}, task_counter_{},
-      task_cond_var_{}, task_mutex_{}, thread_mutex_{}, thread_prioritizer_{[](std::thread&) {}}
+    : done_{false}
     {
         if (n_threads > 0) {
-            std::lock_guard<std::mutex> thread_lock(thread_mutex_);
             const auto n_target = threads_.size() + n_threads;
             while (threads_.size() < n_target) {
                 threads_.emplace_back(&thread_pool::worker, this);
-                thread_prioritizer_(threads_.back());
             }
+        } else {
+            throw transwarp::thread_pool_error{"number of threads must be larger than zero"};
         }
     }
-    /**
-     * Destructor. Joins all threads launched. Waits for all running tasks
-     * to complete
-     */
+
     ~thread_pool() {
         {
-            std::lock_guard<std::mutex> task_lock(task_mutex_);
+            std::lock_guard<std::mutex> lock(mutex_);
             done_ = true;
-            paused_ = false;
         }
-        task_cond_var_.notify_all();
-        std::lock_guard<std::mutex> thread_lock(thread_mutex_);
+        cond_var_.notify_all();
         for (auto& thread : threads_)
             thread.join();
     }
 
-    thread_pool(const thread_pool&) = delete;
-    thread_pool& operator=(const thread_pool&) = delete;
-    thread_pool(thread_pool&&) = delete;
-    thread_pool& operator=(thread_pool&&) = delete;
-    /**
-     * Sets the thread prioritizer to be used when launching new threads.
-     * By default, threads will be launched with the default OS priority
-     */
-    void set_thread_prioritizer(std::function<void(std::thread&)> prioritizer) {
-        thread_prioritizer_ = std::move(prioritizer);
-    }
-    /**
-     * Returns the number of threads launched
-     */
-    std::size_t n_threads() const {
-        {
-            std::lock_guard<std::mutex> task_lock(task_mutex_);
-            if (done_)
-                throw transwarp::thread_pool_error{"n_threads called while thread pool is shutting down"};
+    void prioritize_threads(std::function<void(std::thread&)>& prioritizer) {
+        for (auto& thread : threads_) {
+            prioritizer(thread);
         }
-        std::lock_guard<std::mutex> thread_lock(thread_mutex_);
-        return threads_.size();
     }
-    /**
-     * Pushes a new task into the thread pool. The task will have a priority of 0
-     * @param functor The functor to call
-     * @param args The arguments to pass to the functor when calling it
-     * @return The future associated to the underlying task
-     */
-    template<typename Functor, typename... Args>
-    auto push(Functor&& functor, Args&&... args) -> std::future<decltype(functor(args...))> {
-        return push(0, std::forward<Functor>(functor), std::forward<Args>(args)...);
-    }
-    /**
-     * Pushes a new task into the thread pool while providing a priority
-     * @param priority A task priority. Higher priorities are processed first
-     * @param functor The functor to call
-     * @param args The arguments to pass to the functor when calling it
-     * @return The future associated to the underlying task
-     */
-    template<typename Functor, typename... Args>
-    auto push(std::size_t priority, Functor&& functor, Args&&... args) -> std::future<decltype(functor(args...))> {
-        typedef decltype(functor(args...)) result_type;
-        auto pack_task = std::make_shared<std::packaged_task<result_type()>>(
-                std::bind(std::forward<Functor>(functor), std::forward<Args>(args)...));
-        auto future = pack_task->get_future();
+
+    void push(std::function<void()> functor) {
         {
-            std::lock_guard<std::mutex> task_lock(task_mutex_);
+            std::lock_guard<std::mutex> lock(mutex_);
             if (done_)
                 throw transwarp::thread_pool_error{"push called while thread pool is shutting down"};
-            tasks_.emplace([pack_task]{ (*pack_task)(); }, priority, ++task_counter_);
+            functors_.push(std::move(functor));
         }
-        task_cond_var_.notify_one();
-        return future;
-    }
-    /**
-     * Returns the current number of queued tasks
-     */
-    std::size_t n_tasks() const {
-        std::lock_guard<std::mutex> lock(task_mutex_);
-        return tasks_.size();
-    }
-    /**
-     * Clears all queued tasks. Not affecting currently running tasks
-     */
-    void clear() {
-        std::lock_guard<std::mutex> lock(task_mutex_);
-        decltype(tasks_) empty;
-        tasks_.swap(empty);
-    }
-    /**
-     * Pauses the processing of tasks. Not affecting currently running tasks
-     */
-    void pause() {
-        std::lock_guard<std::mutex> lock(task_mutex_);
-        paused_ = true;
-    }
-    /**
-     * Resumes the processing of tasks
-     */
-    void resume() {
-        {
-            std::lock_guard<std::mutex> lock(task_mutex_);
-            paused_ = false;
-        }
-        task_cond_var_.notify_all();
+        cond_var_.notify_one();
     }
 
 private:
 
     void worker() {
         for (;;) {
-            transwarp::detail::priority_task task;
+            std::function<void()> functor;
             {
-                std::unique_lock<std::mutex> task_lock(task_mutex_);
-                task_cond_var_.wait(task_lock, [this]{
-                    return !paused_ && (done_ || !tasks_.empty());
+                std::unique_lock<std::mutex> lock(mutex_);
+                cond_var_.wait(lock, [this]{
+                    return done_ || !functors_.empty();
                 });
-                if (done_ && tasks_.empty())
+                if (done_ && functors_.empty())
                     break;
-                task = tasks_.top();
-                tasks_.pop();
+                functor = functors_.front();
+                functors_.pop();
             }
-            task.callback()();
+            functor();
         }
     }
 
     bool done_;
-    bool paused_;
     std::vector<std::thread> threads_;
-    std::priority_queue<transwarp::detail::priority_task> tasks_;
-    transwarp::detail::infinite_counter<typename
-    transwarp::detail::priority_task::counter_elem_t> task_counter_;
-    std::condition_variable task_cond_var_;
-    mutable std::mutex task_mutex_;
-    mutable std::mutex thread_mutex_;
-    std::function<void(std::thread&)> thread_prioritizer_;
+    std::queue<std::function<void()>> functors_;
+    std::condition_variable cond_var_;
+    std::mutex mutex_;
 };
 
 template<bool Done, int Total, int... N>
@@ -338,7 +223,7 @@ std::string trim(const std::string &s, const std::string& chars=" \t\n\r") {
 
 inline
 void find_levels(transwarp::node* final) {
-   std::queue<transwarp::node*> q;
+   std::queue<const transwarp::node*> q;
    std::queue<std::size_t> d;
    q.push(final);
    d.push(0);
@@ -348,7 +233,7 @@ void find_levels(transwarp::node* final) {
        const auto depth = d.front(); d.pop();
 
        for (auto n : current->parents) {
-           n->level = depth + 1;
+           const_cast<transwarp::node*>(n)->level = depth + 1;
            q.push(n);
            d.push(n->level);
        }
@@ -363,14 +248,14 @@ struct unvisit_functor {
 };
 
 struct make_edges_functor {
-    make_edges_functor(std::vector<transwarp::edge>& graph, transwarp::node n)
-    : graph_(graph), n_(std::move(n)) {}
+    make_edges_functor(std::vector<transwarp::edge>& graph, transwarp::node& n)
+    : graph_(graph), n_(n) {}
     template<typename Task>
     void operator()(Task* task) const {
-        graph_.push_back({n_, task->get_node()});
+        graph_.push_back({&n_, &task->get_node()});
     }
     std::vector<transwarp::edge>& graph_;
-    transwarp::node n_;
+    transwarp::node& n_;
 };
 
 struct make_parents_functor {
@@ -410,20 +295,6 @@ struct final_visitor {
     std::size_t& id_;
 };
 
-struct schedule_visitor {
-    template<typename Task>
-    void operator()(Task* task) const {
-        auto self = task->shared_from_this();
-        if (task->pool_) {
-            task->future_ = task->pool_->push(task->node_.level, &Task::evaluate, self);
-        } else {
-            auto pkg = std::packaged_task<typename Task::result_type()>(std::bind(&Task::evaluate, self));
-            task->future_ = pkg.get_future();
-            pkg();
-        }
-    }
-};
-
 struct graph_visitor {
     explicit graph_visitor(std::vector<transwarp::edge>& graph)
     : graph_(graph) {}
@@ -432,6 +303,20 @@ struct graph_visitor {
         transwarp::detail::apply(transwarp::detail::make_edges_functor(graph_, task->node_), task->tasks_);
     }
     std::vector<transwarp::edge>& graph_;
+};
+
+struct callback_visitor {
+    explicit callback_visitor(std::priority_queue<transwarp::detail::priority_functor>& queue)
+    : queue_(queue) {}
+    template<typename Task>
+    void operator()(Task* task) const {
+        const auto& node = task->get_node();
+        auto pack_task = std::make_shared<std::packaged_task<typename Task::result_type()>>(
+                              std::bind(&Task::evaluate, task->shared_from_this()));
+        task->future_ = pack_task->get_future();
+        queue_.emplace([pack_task]{ (*pack_task)(); }, node.level, node.id);
+    }
+    std::priority_queue<transwarp::detail::priority_functor>& queue_;
 };
 
 struct set_pool_visitor {
@@ -462,10 +347,10 @@ struct pass_visitor {
 
 inline
 std::string make_dot_graph(const std::vector<transwarp::edge>& graph, const std::string& name="transwarp") {
-    auto info = [](transwarp::node n) {
-        const auto name = transwarp::detail::trim(n.name);
-        return '"' + name + "\nid " + std::to_string(n.id) + " level " + std::to_string(n.level)
-                   + " parents " + std::to_string(n.parents.size()) + '"';
+    auto info = [](const transwarp::node* n) {
+        const auto name = transwarp::detail::trim(n->name);
+        return '"' + name + "\nid " + std::to_string(n->id) + " level " + std::to_string(n->level)
+                   + " parents " + std::to_string(n->parents.size()) + '"';
     };
     std::string dot = "digraph " + name + " {\n";
     for (const auto& pair : graph) {
@@ -492,8 +377,8 @@ public:
     void finalize() {
         std::size_t id = 0;
         transwarp::pass_visitor pass;
-        transwarp::detail::final_visitor post_visitor(id);
-        visit(pass, post_visitor);
+        transwarp::detail::final_visitor pre_visitor(id);
+        visit(pre_visitor, pass);
         unvisit();
         transwarp::detail::find_levels(&node_);
         finalized_ = true;
@@ -502,11 +387,10 @@ public:
     void set_parallel(std::size_t n_threads, std::function<void(std::thread&)> thread_prioritizer=nullptr) {
         check_is_finalized();
         transwarp::pass_visitor pass;
-        wait_for_completion();
         if (n_threads > 0) {
             auto pool = std::make_shared<transwarp::detail::thread_pool>(n_threads);
             if (thread_prioritizer)
-                pool->set_thread_prioritizer(std::move(thread_prioritizer));
+                pool->prioritize_threads(thread_prioritizer);
             transwarp::detail::set_pool_visitor pre_visitor(std::move(pool));
             visit(pre_visitor, pass);
         } else {
@@ -519,12 +403,23 @@ public:
     void schedule() {
         check_is_finalized();
         transwarp::pass_visitor pass;
-        transwarp::detail::schedule_visitor pre_visitor;
-        wait_for_completion();
-        if (pool_) pool_->pause();
-        visit(pre_visitor, pass);
-        if (pool_) pool_->resume();
+
+        std::priority_queue<transwarp::detail::priority_functor> queue;
+        transwarp::detail::callback_visitor post_visitor(queue);
+        visit(pass, post_visitor);
         unvisit();
+
+        if (pool_) {
+            while (!queue.empty()) {
+                pool_->push(queue.top().callback());
+                queue.pop();
+            }
+        } else {
+            while (!queue.empty()) {
+                queue.top().callback()();
+                queue.pop();
+            }
+        }
     }
 
     std::shared_future<result_type> get_future() const {
@@ -576,7 +471,7 @@ private:
     friend struct transwarp::detail::reset_pool_visitor;
     friend struct transwarp::detail::set_pool_visitor;
     friend struct transwarp::detail::graph_visitor;
-    friend struct transwarp::detail::schedule_visitor;
+    friend struct transwarp::detail::callback_visitor;
     friend struct transwarp::detail::final_visitor;
 
     static result_type evaluate(std::shared_ptr<transwarp::task<Functor, Tasks...>> task) {
@@ -588,18 +483,13 @@ private:
             throw transwarp::task_error("task is not finalized: " + node_.name);
     }
 
-    void wait_for_completion() const {
-        if (future_.valid())
-            future_.wait();
-    }
-
     transwarp::node node_;
     Functor functor_;
     std::tuple<std::shared_ptr<Tasks>...> tasks_;
     bool visited_;
     bool finalized_;
-    std::shared_ptr<transwarp::detail::thread_pool> pool_;
     std::shared_future<result_type> future_;
+    std::shared_ptr<transwarp::detail::thread_pool> pool_;
 };
 
 
