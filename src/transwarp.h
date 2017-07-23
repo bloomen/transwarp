757741ca26e23dcc3350ee27ee57c45da396ca26
Christian Blume <chr.blume@gmail.com>
2017-07-23 23:33:20 +1200

    add iexecutable interface

--- src/transwarp.h
+++ src/transwarp.h
@@ -45,14 +45,22 @@ struct edge {
 };
 
 
+// An interface for gaining access to member functions that do not depend
+// on the result type of the task
+class iexecutable {
+public:
+    virtual ~iexecutable() = default;
+    virtual void set_executor(std::shared_ptr<transwarp::executor> executor) = 0;
+    virtual const transwarp::node& get_node() const = 0;
+};
+
+
 // An interface for the task class
 template<typename ResultType>
-class itask {
+class itask : public iexecutable {
 public:
     virtual ~itask() = default;
-    virtual void set_executor(std::shared_ptr<transwarp::executor> executor) = 0;
     virtual std::shared_future<ResultType> get_future() const = 0;
-    virtual const transwarp::node& get_node() const = 0;
 };
 
 
