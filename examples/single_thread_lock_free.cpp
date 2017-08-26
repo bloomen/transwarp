96ddedc8f5ecb8af89e26bb6e3beefeaed21e7ae
Christian Blume <chr.blume@gmail.com>
2017-08-27 01:00:22 +1200

    add lock free queue

--- examples/single_thread_lock_free.cpp
+++ examples/single_thread_lock_free.cpp
@@ -1,11 +1,39 @@
 #include "single_thread_lock_free.h"
 #include "../src/transwarp.h"
+#include "readerwriterqueue.h"
 #include <fstream>
 #include <iostream>
 
 namespace tw = transwarp;
 
 namespace examples {
+
+namespace {
+
+class lock_free_executor : public tw::executor {
+public:
+    explicit lock_free_executor()
+    : thread_(&lock_free_executor::worker, this), queue_(100) {}
+
+    void execute(const std::function<void()>& functor, const std::shared_ptr<transwarp::node>&) {
+        queue_.enqueue(functor);
+    }
+
+private:
+
+    void worker() {
+        for (;;) {
+            std::function<void()> functor;
+            queue_.wait_dequeue(functor);
+            functor();
+        }
+    }
+
+    std::thread thread_;
+    moodycamel::BlockingReaderWriterQueue<std::function<void()>> queue_;
+};
+
+}
 
 void single_thread_lock_free(std::ostream& os) {
     (void)os;
