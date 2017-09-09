ef5980195998f823a5cfddb22f574f1c2c0a3c55
Christian Blume <chr.blume@gmail.com>
2017-09-09 20:04:55 +1200

    fix script

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cppcheck --template="{file};{line};{severity};{id};{message}" --quiet --enable=all \
---std=c99 --std=c++11 --error-exitcode=1 --relative-paths --suppress=missingIncludeSystem \
---inline-suppr $thisdir
+output=$(cppcheck --template="{file};{line};{severity};{id};{message}" --quiet --enable=all \
+--std=c99 --std=c++11 --relative-paths --suppress=missingIncludeSystem \
+--suppress=syntaxError --inline-suppr $thisdir 2>&1)
+if [ ! -z "$output" ];then
+    echo $output
+    exit 1  
+fi 
