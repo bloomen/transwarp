59f286d4b8e7a81973adbdfa4e0a873bf4f29477
Christian Blume <chr.blume@gmail.com>
2017-09-14 22:24:34 +1200

    enable valgrind on linux only

--- valgrind.sh
+++ valgrind.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
+if [ "$(uname)" = "Linux" ];then  # valgrind currently buggy on Mac
 valgrind --quiet --tool=memcheck --leak-check=full --show-reachable=yes \
 --num-callers=20 --error-exitcode=1 $1
+fi
