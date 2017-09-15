88ebb6003c46ba3bdd7baffeb06c63c6363674b8
Christian Blume <chr.blume@gmail.com>
2017-09-16 00:25:16 +1200

    fix it

--- cppcheck.sh
+++ cppcheck.sh
@@ -4,7 +4,7 @@ thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 output=$(cppcheck --template="{file};{line};{severity};{id};{message}" --quiet --enable=all \
 --std=c99 --std=c++11 --relative-paths --suppress=missingIncludeSystem \
 --suppress=syntaxError --inline-suppr $thisdir 2>&1)
-if [ ! -z "$output" ];then
+if [ "x$output" != "x" ];then
     echo $output
     exit 1  
 fi 
