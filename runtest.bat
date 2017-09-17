724091f98869f5e63e9ce15daebab5a583d7737a
Christian Blume <chr.blume@gmail.com>
2017-09-17 15:04:54 +1200

    ensure similar builds

--- runtest.bat
+++ runtest.bat
@@ -1,7 +1,7 @@
 @echo off
 echo "+++ Running %1"
 FOR /L %%i IN (1,1,100) DO (
-    %1 > nul
+    %1 --use-colour no --order rand --rng-seed time > nul
 	if %errorlevel% neq 0 exit /b %errorlevel%
 )
 echo "Tests OK"
