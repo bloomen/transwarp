1cdb9e6e93c2babbc4a46aa5aa1a27f1a801398e
Christian Blume <chr.blume@gmail.com>
2017-09-17 15:45:27 +1200

    not doing this bat crap

--- runtest.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo "+++ Running %1"
-FOR /L %%i IN (1,1,100) DO (
-    %1 --use-colour no --order rand --rng-seed "time" 
-	if %errorlevel% neq 0 exit /b %errorlevel%
-)
-echo "Tests OK"
