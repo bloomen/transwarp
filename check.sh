46ce8a2929e9ea673a4a82e8562deffc1f0cd61a
Christian Blume <chr.blume@gmail.com>
2017-10-07 14:09:01 +1300

    add script to check all

---
+++ check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$thisdir/cppcheck.sh
+
+modes='Debug Release'
+
+function run_many_times() {
+    echo "+++ Running tests many times ..."
+    count=0
+    while [ $count -lt 100 ]; do
+         ./transwarp_test --use-colour no --order rand --rng-seed 'time' > /dev/null
+        let count+=1
+    done
+    echo "Tests OK"
+}
+
+for mode in $modes; do
+    echo "+++ Checking $mode ..."
+    dir=$thisdir/check_build_$mode
+    rm -rf $dir
+    mkdir $dir
+    cd $dir
+    echo "Building ..."
+    cmake -DCMAKE_BUILD_TYPE=$mode .. > /dev/null
+    make -j4 > /dev/null
+    echo "Running ..."
+    ctest --verbose
+    run_many_times
+    $thisdir/valgrind.sh $dir/transwarp_test --use-colour no
+    cd $thisdir    
+    rm -rf $dir
+done
