4583f3886fe0933cea406f9d2331d9810cc08e65
Christian Blume <chr.blume@gmail.com>
2018-03-14 23:38:46 +1300

    remove unmatched suppression

--- cppcheck.sh
+++ cppcheck.sh
@@ -4,7 +4,7 @@ thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "+++ Running CppCheck ..."
 output=$(cppcheck --template="{file};{line};{severity};{id};{message}" --quiet --enable=all \
 --std=c99 --std=c++11 --relative-paths --suppress=missingIncludeSystem \
---suppress=syntaxError --suppress=preprocessorErrorDirective --force --inline-suppr $thisdir)
+--suppress=syntaxError --force --inline-suppr $thisdir)
 if [ "x$output" != "x" ];then
     echo "CppCheck Failed:"
     echo $output
