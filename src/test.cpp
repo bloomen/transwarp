683a01a7d760341e426c5e23415a9fbc1fa917bd
Christian Blume <chr.blume@gmail.com>
2018-04-14 22:51:51 +1200

    back to visual studio 15

--- src/test.cpp
+++ src/test.cpp
@@ -1212,6 +1212,8 @@ TEST_CASE("value_task_with_changing_value") {
     REQUIRE(42 == t->get());
 }
 
+#ifndef _WIN32
+
 TEST_CASE("value_task_with_ref_value") {
     int z = 42;
     int& x = z;
@@ -1251,7 +1253,6 @@ TEST_CASE("value_task_with_rvalueref_value") {
     REQUIRE(x == t->get());
 }
 
-#ifndef _WIN32
 TEST_CASE("value_task_with_ref_wrapper_value") {
     int x = 42;
     auto t = make_value_task(std::ref(x));
@@ -1259,6 +1260,7 @@ TEST_CASE("value_task_with_ref_wrapper_value") {
     x = 43;
     REQUIRE(x == t->get().get());
 }
+
 #endif
 
 // Examples
