82c18ca22420a87eff4b958c5832bbcbe93fe821
Christian Blume <chr.blume@gmail.com>
2018-04-19 08:00:55 +1200

    clean up conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,21 @@
-from   conans       import ConanFile, CMake, tools
-from   conans.tools import download, unzip
+from conans import ConanFile
+from conans.tools import download, unzip
 import os
 
+
 class TranswarpConan(ConanFile):
-    name            = "transwarp"
-    version         = "1.3.0"                     
-    description     = "Conan package for bloomen/transwarp."           
-    url             = "https://github.com/bloomen/transwarp"
-    license         = "MIT"							
-    settings        = "arch", "build_type", "compiler", "os"
-    generators      = "cmake"
+    name = "transwarp"
+    version = "1.3.0"
+    description = "Conan package for bloomen/transwarp."
+    url = "https://github.com/bloomen/transwarp"
+    license = "MIT"
+    settings = "arch", "build_type", "compiler", "os"
+    generators = "cmake"
 
     def source(self):
         zip_name = "%s.zip" % self.version
-        download ("%s/archive/%s" % (self.url, zip_name), zip_name, verify=False)
-        unzip    (zip_name)
+        download("%s/archive/%s" % (self.url, zip_name), zip_name, verify=False)
+        unzip(zip_name)
         os.unlink(zip_name)
 
     def package(self):
