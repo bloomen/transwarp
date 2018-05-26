962006de5843db727ed05cc584948870a5a28946
Christian Blume <chr.blume@gmail.com>
2018-05-26 15:56:19 +1200

    use docs output folder

--- html/search/functions_6e.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['node',['node',['../classtranswarp_1_1node.html#a1b09ec39b786f8ce5b894ad60041ad3f',1,'transwarp::node']]]
-];
--- html/search/functions_70.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['parallel',['parallel',['../classtranswarp_1_1parallel.html#a70042b5a95189edd126dddd4927f5249',1,'transwarp::parallel']]]
-];
