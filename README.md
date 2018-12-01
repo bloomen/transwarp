eed5d23d87877e852149e4c6f1f6fad25b050091
Christian Blume <chr.blume@gmail.com>
2018-12-01 20:07:00 +1300

    adjust the basic example

--- README.md
+++ README.md
@@ -61,15 +61,7 @@ int main() {
     auto task2 = tw::make_value_task("something else", 42);
     auto task3 = tw::make_task(tw::consume, "adder", adder, task1, task2);
 
-    // Creating a dot-style graph for visualization
-    const auto graph = task3->get_graph();
-    std::ofstream("basic_with_three_tasks.dot") << tw::to_string(graph);
-
-    // schedule() can now be called as much as desired. The task graph
-    // only has to be built once
-
-    // Parallel execution with 4 threads for independent tasks
-    tw::parallel executor{4};
+    tw::parallel executor{4};  // Parallel execution with 4 threads
 
     task3->schedule_all(executor);  // Schedules all tasks for execution
     std::cout << "result = " << task3->get() << std::endl;  // result = 55.3
@@ -80,6 +72,10 @@ int main() {
 
     task3->schedule_all(executor);  // Re-schedules all tasks for execution
     std::cout << "result = " << task3->get() << std::endl;  // result = 58.8
+
+    // Creating a dot-style graph for visualization
+    const auto graph = task3->get_graph();
+    std::ofstream("basic_with_three_tasks.dot") << tw::to_string(graph);
 }
 ```
 
--- examples/basic_with_three_tasks.cpp
+++ examples/basic_with_three_tasks.cpp
@@ -21,15 +21,7 @@ void basic_with_three_tasks(std::ostream& os) {
     auto task2 = tw::make_value_task("something else", 42);
     auto task3 = tw::make_task(tw::consume, "adder", adder, task1, task2);
 
-    // Creating a dot-style graph for visualization
-    const auto graph = task3->get_graph();
-    std::ofstream("basic_with_three_tasks.dot") << tw::to_string(graph);
-
-    // schedule() can now be called as much as desired. The task graph
-    // only has to be built once
-
-    // Parallel execution with 4 threads for independent tasks
-    tw::parallel executor(4);
+    tw::parallel executor{4};  // Parallel execution with 4 threads
 
     task3->schedule_all(executor);  // Schedules all tasks for execution
     os << "result = " << task3->get() << std::endl;  // result = 55.3
@@ -40,6 +32,10 @@ void basic_with_three_tasks(std::ostream& os) {
 
     task3->schedule_all(executor);  // Re-schedules all tasks for execution
     os << "result = " << task3->get() << std::endl;  // result = 58.8
+
+    // Creating a dot-style graph for visualization
+    const auto graph = task3->get_graph();
+    std::ofstream("basic_with_three_tasks.dot") << tw::to_string(graph);
 }
 
 }
