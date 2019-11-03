e21b6c0a45f28cd236e1ba5e8050d3463a854295
Christian Blume <chr.blume@gmail.com>
2019-11-04 00:39:43 +1300

    bump version

--- src/transwarp.h
+++ src/transwarp.h
@@ -1,6 +1,6 @@
 /// @mainpage transwarp is a header-only C++ library for task concurrency
 /// @details https://github.com/bloomen/transwarp
-/// @version 2.1.0
+/// @version 2.2.0
 /// @author Christian Blume, Guan Wang
 /// @date 2019
 /// @copyright MIT http://www.opensource.org/licenses/mit-license.php
